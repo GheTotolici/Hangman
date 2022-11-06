1adca595ec4aec42785fbcb137ba7d290f32dc0c
G.Totolici <116436379+GheTotolici@users.noreply.github.com>
2022-11-06 10:25:27 +0100

    Update Hangman.js

--- Hangman.js
+++ Hangman.js
@@ -1,33 +1,34 @@
-let currWord, currMoves;
-function theWord() {
-    currWord = document.getElementById("inputWord").value;
-    currMoves = currWord.length + 3;
-    if (!/[^a-zA-Z]/.test(currWord)) {
-        alert("Player2 you have " + currMoves + " moves to find the word.");
+let word, guessingMoves;
+function wordToGuess() {
+    word = document.getElementById("inputWord").value;
+    guessingMoves = word.length + 3;
+    if (!/[^a-zA-Z]/.test(word)) {
+        alert("Player2 you have " + guessingMoves + " moves to find the word.");
     } else {
         alert("The word must contain only LETTERS, Please enter another word.")
     }
     document.getElementById("inputWord").value = "";
 }
 
-function findLetter() {
+function letterToCompare() {
     var currLetter = document.getElementById("letter").value;
-    --currMoves;
-    for (let i = 0; i < currWord.length; ++i) {
-        if (currWord[i] == currLetter && currMoves >= 0) {
-            currWord = currWord.replace(currWord[i], '*');
+    --guessingMoves;
+    for (let i = 0; i < word.length; ++i) {
+        if (word[i] == currLetter && guessingMoves >= 0) {
+            word = word.replace(word[i], '*');
         } 
     }
-    if (currMoves >= 0 && !(currWord.match(/[a-zA-Z]/))) {
+    if (guessingMoves >= 0 && !(word.match(/[a-zA-Z]/))) {
          alert("Congratulations YOU HAVE WON.");
-    } else if (!currMoves) {
+    } else if (!guessingMoves) {
         alert("I am sorry but YOU LOST.Reset the Game and start again.");
         document.location.reload();
     } else {
-        alert("Player2 YOU have  " + currMoves + " moves left to find the word.");
+        alert("Player2 YOU have  " + guessingMoves + " moves left to find the word.");
     }
     document.getElementById("letter").value = "";
 }
+
 function reset() {
     document.location.reload();
 }
