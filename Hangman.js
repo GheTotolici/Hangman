f98613e3c05dd4cbcdec79d0359feea9fc6822fc
G.Totolici <116436379+GheTotolici@users.noreply.github.com>
2022-11-07 20:35:48 +0100

    Update Hangman.js

--- Hangman.js
+++ Hangman.js
@@ -1,11 +1,12 @@
 let word, guessingMoves;
+let showMessaje = document.querySelector("h2");
 function wordToGuess() {
     word = document.getElementById("inputWord").value;
     guessingMoves = word.length + 3;
     if (!/[^a-zA-Z]/.test(word)) {
-        alert("Player2 you have " + guessingMoves + " moves to find the word.");
+        showMessaje.innerHTML = ("Player2 you have " + guessingMoves + " moves to find the word.");
     } else {
-        alert("The word must contain only LETTERS, Please enter another word.")
+        showMessaje.innerHTML = ("The word must contain only LETTERS, Please enter another word.")
     }
     document.getElementById("inputWord").value = "";
 }
@@ -19,12 +20,12 @@ function letterToCompare() {
         } 
     }
     if (guessingMoves >= 0 && !(word.match(/[a-zA-Z]/))) {
-         alert("Congratulations YOU HAVE WON.");
+         showMessaje.innerHTML = ("Congratulations YOU HAVE WON.");
     } else if (!guessingMoves) {
-        alert("I am sorry but YOU LOST.Reset the Game and start again.");
+        messaje.innerHTML = ("I am sorry but YOU LOST.Reset the Game and start again.");
         document.location.reload();
     } else {
-        alert("Player2 YOU have  " + guessingMoves + " moves left to find the word.");
+        showMessaje.innerHTML = ("Player2 YOU have  " + guessingMoves + " moves left to find the word.");
     }
     document.getElementById("letter").value = "";
 }
